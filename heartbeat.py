420bb8feb96c7db2f219e84b8fe46679c1834f59
Denis Lavrov <bahus.vel@gmail.com>
2015-12-28 14:23:52 +0900

    Create heartbeat.py

---
+++ heartbeat.py
@@ -0,0 +1,23 @@
+__author__ = 'denislavrov'
+from .api import Message, CORModule
+import threading
+import time
+
+
+class Heartbeat(CORModule):
+	def snort(self):
+		while True:
+			time.sleep(self.period)
+			self.messageout(Message("HEARTBEAT", {"timestamp": time.clock()}))
+
+	def rx_heartbeat(self, message):
+		print("Received heartbeat from: " + message.source)
+
+	def __init__(self, period=10, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.period = period
+		self.add_topics({"HEARTBEAT": self.rx_heartbeat})
+		self.subtree = []
+		self.t = threading.Thread(target=self.snort)
+		self.t.start()
+		print("Initializing Heartbeat " + str(self.mid))
